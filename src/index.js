fa5357649882501aaec555745cd1304c9f144309
Ilya Kulish <ilyakulish06@outlook.com>
2023-04-25 02:43:36 +0400

    code

---
+++ src/index.js
@@ -0,0 +1,91 @@
+const API_KEY = "35738643-c5bfe559c2bde2990bd981af5";
+const searchForm = document.querySelector("#search-form");
+const loadMoreBtn = document.querySelector(".load-more");
+const galleryList = document.querySelector(".gallery");
+
+let searchQuery = "";
+let page = 1;
+
+const createGalleryItemMarkup = (image) => {
+  const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = image;
+
+  return `<div class="photo-card">
+      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      <div class="stats">
+        <p class="stats-item">
+          <i class="material-icons">thumb_up</i>
+          ${likes}
+        </p>
+        <p class="stats-item">
+          <i class="material-icons">visibility</i>
+          ${views}
+        </p>
+        <p class="stats-item">
+          <i class="material-icons">comment</i>
+          ${comments}
+        </p>
+        <p class="stats-item">
+          <i class="material-icons">cloud_download</i>
+          ${downloads}
+        </p>
+      </div>
+    </div>`;
+};
+
+const fetchImages = () => {
+  const url = `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
+
+  return fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      throw new Error("Network response was not ok");
+    })
+    .then((data) => {
+      const images = data.hits;
+
+      if (images.length === 0) {
+        Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.");
+      } else {
+        const galleryItemsMarkup = images.map((image) => createGalleryItemMarkup(image)).join("");
+        galleryList.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+        if (images.length < 40) {
+          Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+          loadMoreBtn.classList.add("is-hidden");
+        } else {
+          loadMoreBtn.classList.remove("is-hidden");
+        }
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching images:", error);
+      Notiflix.Notify.failure("Oops! Something went wrong. Please try again later.");
+    });
+};
+
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const input = event.currentTarget.elements.searchQuery;
+
+  if (!input.value) {
+    return;
+  }
+
+  searchQuery = input.value.trim();
+  page = 1;
+
+  galleryList.innerHTML = "";
+  loadMoreBtn.classList.add("is-hidden");
+
+  fetchImages();
+});
+
+loadMoreBtn.addEventListener("click", () => {
+  page += 1;
+
+  fetchImages();
+});
